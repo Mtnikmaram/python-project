0d5f5118b4c7e417dbd7ba2aa261e0aa04130e7f
Mtnikmaram <mtnikmaram@gmail.com>
2023-03-01 17:23:48 +0330

    making the class board to start the project

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {"computer": 0 , "user": 0}
+
+class board
+    """ this is for the main board size and the ships"""
+    def __init__(self, size , ship_num , name, type):
+        self.size = size
+        self.ship_num = ship_num
+        self.name = name
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.type = type
+        self.guesses = []
+        self.ship = []
+
+    
+        
